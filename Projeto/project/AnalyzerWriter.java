88afaa74f301c027f7790e4d3ed95544eaf7e9ee
skypillar <62655287+Skypillars@users.noreply.github.com>
2023-09-29 16:22:07 -0300

    Update AnalyzerWriter.java

--- Projeto/project/AnalyzerWriter.java
+++ Projeto/project/AnalyzerWriter.java
@@ -14,11 +14,19 @@ public class AnalyzerWriter {
     private String fileName;
     private Map<String, List<String>> digraph;
 
+    /**Método construtor da classe, chamando o nome do arquivo e dígrafo gerado
+    * @param fileName String - Nome do arquivo
+    * @param digraph Map<String, List<String>> - Dígrafo gerado pela classe AnalyzerReader
+    */
+    
     public AnalyzerWriter(String fileName, Map<String, List<String>> digraph) {
         this.fileName = fileName;
         this.digraph = digraph;
     }
 
+    /**Método para converter o dígrafo para csv, escrevendo uma mensagem no console no caso de sucesso do processo
+    */
+    
     public void writeToCSV() throws IOException {
         String outputFileName = fileName.replace(".txt", ".csv");
 
