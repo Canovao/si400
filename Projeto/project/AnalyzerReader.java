06bc8703b10c3d7075cb29d9084a2e887b17926a
skypillar <62655287+Skypillars@users.noreply.github.com>
2023-09-29 16:13:01 -0300

    Update AnalyzerReader.java
    JavaDoc adicionado

--- Projeto/project/AnalyzerReader.java
+++ Projeto/project/AnalyzerReader.java
@@ -3,14 +3,28 @@ package project;
 import java.io.*;
 import java.util.*;
 
+/**Classe para ler o arquivo passado e gerar o dígrafo, contendo como atributo o nome do arquivo e métodos necessários
+* @author Nicolas Canova
+* @version 1.0
+* @since Primeira versão da aplicação
+*/
+
 public class AnalyzerReader {
 
     private String fileName;
 
+    /**Método construtor da classe, chamando o nome do arquivo 
+    * @param fileName String - Nome do arquivo para leitura
+    */
+    
     public AnalyzerReader(String fileName) {
         this.fileName = fileName;
     }
 
+    /**Método para leitura do arquivo e geração do dígrafo
+    * @return Map<String, List<String>> - Dígrafo gerado
+    */
+    
     public Map<String, List<String>> generateDigraph() throws IOException {
         Map<String, List<String>> digraph = new TreeMap<>();
 
